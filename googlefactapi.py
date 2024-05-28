547b43da3f372487c9af963f266f649904a4f19c
Salem <salemiranloye@gmail.com>
2024-05-28 13:55:17 -0500

    nltk stopword removal

--- googlefactapi.py
+++ googlefactapi.py
@@ -1,8 +1,13 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import requests
 from dotenv import load_dotenv
 import os
 import json
+
 load_dotenv()
+
+stop_words = set(stopwords.words('english'))
 
 claims = [
     "Climate Change",
@@ -16,23 +21,29 @@ presets = {
     'offset': None,
 }
 
-
 class GoogleApi:
     def __init__(self) -> None:
         self.key = os.getenv('GOOGLE_KEY')
         self.url = 'https://factchecktools.googleapis.com/v1alpha1/claims:search'
         # https://toolbox.google.com/factcheck/api/search other url thats not public technically
-    
+
+    def stopword_removal(self,sentence):
+        word_tokens = word_tokenize(sentence)
+        filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+        return " ".join(filtered_sentence)
+
     def search_claim(self,query,singleUse=True):
+        filtered_sentence  = self.stopword_removal(query)
+        print(filtered_sentence)
         params = {
-        'query': query,
+        'query': filtered_sentence,
         'key': self.key
         }
-        
+
         for key, value in presets.items():
             if value is not None:
                 params[key] = value
-        
+
         response = requests.get(self.url, params=params)
         if response.status_code == 200:
             if not singleUse:
@@ -41,22 +52,22 @@ class GoogleApi:
                 filename = f'search_claim_{query}.json'
                 with open(filename, 'w') as outfile:
                     json.dump(response.json(), outfile)
-        
+
     def group_search_claims(self,querys):
         count = 0
         filename = f'group_search_{count}.json'
         while os.path.exists(filename):
             count += 1
             filename = f"group_search_{count}.json"
-        
+
         group_results = {}
         for claim in querys:
             data = self.search_claim(claim, singleUse=False)
             if data:
                 group_results[claim] = data
-        
+
         with open(filename, 'w') as outputfile:
             json.dump(group_results, outputfile)
 
 api = GoogleApi()
-api.group_search_claims(claims)
+api.search_claim("Donald Trump deported people than Barack Obama did when he was president.")
